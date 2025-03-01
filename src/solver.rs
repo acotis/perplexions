9dd9e2ef6b671481932a6e81f9d6aa051d27c40e
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2025-03-01 00:51:05 -0500

    Setting up to rigorously solve real puzzles.

--- src/solver.rs
+++ src/solver.rs
@@ -123,7 +123,7 @@ impl LevelSolver {
 
     fn explore(&mut self, seen: &mut HashSet<String>, blessed: &mut LiveList, context: &mut Vec<String>) {
         // short circuit
-        if context.len() > 2 {return;}
+        // if context.len() > 2 {return;}
 
         let context_str = context.join(" ");
 
@@ -169,7 +169,7 @@ impl Display for LevelSolver {
 fn main() {
     constants::initialize();
 
-    for level in constants::levels() {
+    for level in constants::levels().skip(14) {
         let mut solver = LevelSolver::new(level);
         let mut blessed = LiveList::new("src/blessed_words.txt");
 
