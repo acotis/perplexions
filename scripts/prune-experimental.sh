1b8633561122c157dc097cc23643bdf44879aa6b
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2026-06-30 12:01:20 -0700

    Added experimental level; improved scripts; word work.

--- scripts/prune-experimental.sh
+++ scripts/prune-experimental.sh
@@ -1,2 +1,2 @@
-prune-words -w words.txt -l levels-experimental.txt -b blessed_words.txt
+prune-words -w ./words-and-levels/words.txt -l ./words-and-levels/levels-experimental.txt -b ./words-and-levels/blessed_words.txt
 
--- scripts/prune-official.sh
+++ scripts/prune-official.sh
@@ -1,1 +1,1 @@
-prune-words -w words.txt -l levels.txt -b blessed_words.txt
+prune-words -w ./words-and-levels/words.txt -l ./words-and-levels/levels.txt -b ./words-and-levels/blessed_words.txt
