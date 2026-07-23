a7d1a7a4ab41e6ae9240b2acbbe1fcd15b9ea104
Evan Rysdam <6502776+acotis@users.noreply.github.com>
2026-07-23 05:34:00 -0700

    Updated level-prepending script.

--- scripts/prepend-new-level.sh
+++ scripts/prepend-new-level.sh
@@ -1,10 +1,6 @@
 
-cd ../level-gen
-#cargo run -- --shape 3,2,4,4
-cargo run -- "$@"
-cd ../perplexions
 echo "——————————" > /tmp/experimental-levels.txt
-cat ../level-gen/levels-experimental.txt       >> /tmp/experimental-levels.txt
+level-gen --words --orthogonal --dictionary ./words-and-levels/words.txt >> /tmp/experimental-levels.txt
 cat ./words-and-levels/experimental-levels.txt >> /tmp/experimental-levels.txt
 mv /tmp/experimental-levels.txt ./words-and-levels/experimental-levels.txt
 
